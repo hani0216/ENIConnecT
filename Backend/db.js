61896685daf108d102ef2a12020a5739e1431f9a
hani0216 <hani.mediouni@etudiant-enit.utm.tn>
2024-11-22 22:10:54 +0100

    connect db try

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EniConnecT', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect('mongodb+srv://adminenit:adminenit@eniconnect.ewroc.mongodb.net/?retryWrites=true&w=majority&appName=ENIConnecT', {
+
 }).then(() => {
     console.log('Connexion à la base de données locale MongoDB réussie');
 }).catch(err => {
     console.error('Erreur de connexion à la base de données MongoDB locale', err);
 });
+
