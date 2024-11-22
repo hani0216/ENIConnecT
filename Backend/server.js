ab8e542bd19467ee62da804b112580412288d990
hani0216 <hani.mediouni@etudiant-enit.utm.tn>
2024-11-22 23:24:23 +0100

    Update server

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,15 @@
-const express = require('express');
-const mongoose = require('./config/db'); 
+const express = require('express');  // Importation d'Express
+const app = express();               // Création de l'application Express
 
-const app = express();
+// Configuration du port (valeur fixe)
+const port = 3000;
+
+// Route de base
+app.get('/', (req, res) => {
+  res.send('Serveur Node.js sans dotenv fonctionne !');
+});
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Serveur démarré sur le port ${port}`);
+});
