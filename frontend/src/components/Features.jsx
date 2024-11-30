745ed815dd07e103256d0bd1170d7ec632566c30
hani0216 <hani.mediouni@etudiant-enit.utm.tn>
2024-11-30 20:14:58 +0100

    features update

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import '../styles/Features.css'
+import Separation from './Separtion'
 
 function Features() {
   return (
     <div>
-        <div className='main-container'>
-      <span className='features'>Fonctionnalités</span>
-      <div className='line' />
-      <div className='line-1' />
-    </div>
-
-
-
-
+        <Separation title={"Fonctionnalités"} />
     </div>
   )
 }
---
+++ frontend/src/components/Separtion.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Separation.css'
+
+function Features({ title }) {
+  return (
+    <div className='Features'>
+      <div className='main-container1'>
+        <div className='line' />
+        <span className='features'>{title}</span>
+        <div className='line' />
+      </div>
+    </div>
+  );
+}
+
+export default Features;
