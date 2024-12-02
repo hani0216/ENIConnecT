c42f802339afcc9f245fc1cab2cac02f2aad8cde
hani0216 <hani.mediouni@etudiant-enit.utm.tn>
2024-12-02 10:16:51 +0100

    update

--- frontend/src/components/HomeFooter.jsx
+++ frontend/src/components/HomeFooter.jsx
@@ -6,19 +6,19 @@ function HomeFooter() {
     <div className='Footer1'>
         <section className='logo2'></section>
         <section className='copyright'>
-            <h1>🤝 Contribution et Open Source📚</h1>
-        <h5>Ce projet a été réalisé par un groupe d'étudiants de la filière informatique de l'ENIT. Notre objectif est d'améliorer la vie associative et de renforcer la collaboration entre les étudiants. Nous croyons en l'open source et souhaitons offrir à la communauté un outil évolutif et participatif.
+            <h3>🤝 Contribution et Open Source📚</h3>
+        <h7>Ce projet a été réalisé par un groupe d'étudiants de la filière informatique de l'ENIT. Notre objectif est d'améliorer la vie associative et de renforcer la collaboration entre les étudiants. Nous croyons en l'open source et souhaitons offrir à la communauté un outil évolutif et participatif.
 
         Le code source de cette plateforme est ouvert à tous ceux qui souhaitent contribuer et l'améliorer. Vos idées et contributions sont les bienvenues pour enrichir ENIConnect.
 
-        Pour toute question ou suggestion, n'hésitez pas à nous contacter. Ensemble, construisons une plateforme meilleure et plus collaborative !</h5>
+        Pour toute question ou suggestion, n'hésitez pas à nous contacter. Ensemble, construisons une plateforme meilleure et plus collaborative !</h7>
         
         </section>
             <section className='contact'>
-            <h1>Coordonnées de contact</h1>
-        <h5>Téléphone: +216 50 231 860</h5>
-        <h5>E-mail: Eniconnect@enit.utm.tn</h5>
-        <h5>Ecole nationale d'ingénieurs de Tunis , Manar 1 , Tunis</h5>
+            <h3>Coordonnées de contact</h3><br />
+        <h7>Téléphone: +216 50 231 860</h7> <br />
+        <h7>E-mail: Eniconnect@enit.utm.tn</h7><br />
+        <h7>Ecole nationale d'ingénieurs de Tunis , Manar 1 , Tunis</h7>
         </section>
         
         </div>
